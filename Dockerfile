d8bbff0fa8688980bc7d40d85cc42ceb6551c72f
Orla Fahy <c13520633@mydit.ie>
2015-12-09 15:22:25 +0000

    Changes to code

--- Dockerfile
+++ Dockerfile
@@ -11,19 +11,16 @@ RUN apt-get update
 RUN apt-get -y upgrade
 
 # Copy the application folder inside the container
-ADD /sqs_application /my_application
+ADD /my_application /my_application
 
 # Get pip to download and install requirements:
 RUN pip install -r /my_application/requirements.txt
-RUN pip install boto
-
 
 # Expose listener port
 EXPOSE 5000
-RUN mkdir /data
 
 # Set the default directory where CMD will execute
 WORKDIR /my_application
-# Set the default command to execute    
+
 # when creating a new container
 CMD python server.py
--- my_application/server.py
+++ my_application/server.py
@@ -1,148 +1,154 @@
 import os
-import json
-import requests
-
+from flask import Flask, Response, request, redirect, url_for
+from werkzeug import secure_filename
+import urllib2
+import boto
 import boto.sqs
 import boto.sqs.queue
 from boto.sqs.message import Message
 from boto.sqs.connection import SQSConnection
 from boto.exception import SQSError
-
-from flask import Flask, request, redirect, url_for
-from werkzeug import secure_filename
-
-UPLOAD_FOLDER = '/data/'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+import sys
+import json
+from tempfile import mkdtemp
+from subprocess import Popen, PIPE
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-def connect():
-
-    res = requests.get('http://ec2-52-30-7-5.eu-west-1.compute.amazonaws.com:81/key')
-    keys = res.text.split(':')
-
-    # Get the keys from a specific url and then use them to connect to AWS Service 
-    access_key_id = keys[0]
-    secret_access_key = keys[1]
-
-    # Set up a connection to the AWS service. 
-    return boto.sqs.connect_to_region("eu-west-1", aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key)
-
 
 @app.route("/")
 def index():
-   return "ay"
-
-
-@app.route('/queues', methods=['GET'])
-def list_all_queues():
-    conn = connect()
-
-    rs = conn.get_all_queues()
-
-    queues = []
-
-    for q in rs:
-        queue = {}
-        queue['name'] = q.id
-        queues.append(queue)
-
-    return json.dumps(queues)
-
-@app.route('/queues', methods=['POST'])
-def create_queue():
-
-    body = request.get_json(force=True)
-    name = body['name'] 
-
-    conn = connect()
-    queue = conn.create_queue(name)
-
-    resp = {}
-    resp['name'] = queue.name
-
-    return json.dumps(resp)
-
-
-@app.route('/queues/<name>', methods=['DELETE'])
-def delete_queue(name):
-
-    conn = connect()
-
-    try:
-        queue = conn.get_queue(name)
-
-        if conn.delete_queue(queue):
-            return "Queue deleted: " + name
-        else:
-            return "Failed to delete: " + name
-
-    except:
-        return "Queue does not exist: " + name
-
-
-@app.route('/queues/<name>/msgs/count', methods=['GET'])
-def count_messages(name):
-    conn = connect()
-
-    # Get the queue object to count the messages
-    queue = conn.get_queue(name)
-
-    # Count the messages
-    if queue is not None:
-        attr = queue.get_attributes()
-        return "Number of messages in " + queue.name + ": " + attr['ApproximateNumberOfMessages']
-    else:
-        return "Queue could not be found"
-
-
-@app.route('/queues/<name>/msgs', methods=['POST'])
-def write_message(name):
-
-    body = request.get_json(force=True)
-    message = body['content'] 
-
-    conn = connect()
-    queue = conn.get_queue(name)
+    return 
     
-    # Send the message
-    if queue is not None:
-        conn.send_message(queue, message)
-        return "Sent message to: " + queue.name
-    else:
-        return "Unable to send message"
+"""
+Available API endpoints:
+GET /queues						List all queues
+POST /queues					Create a new queue
+DELETE /queues/<id>				Delete a specific queue
+GET /queues/<id>/msgs			Get a message, return it to the user
+GET /queues/<qud>/msg/count		Return the number of messages in a queue
+POST /queues/<qid>/msgs 		Write a new message to a queue
+DELETE /queues/<qid>/msgs 		Get and delete a message from the queue
+"""
+
+@app.route("/version", methods=['GET'])
+def version():
+	"""
+	print boto version
+	curl -s -X GET localhost:5000/version
+	"""
+	print("Boto version: "+boto.Version+ "\n")
+	return "Boto version: "+boto.Version+ "\n"
+
+@app.route("/queues", methods=['GET'])
+def queues_index():
+	"""
+	List all queues
+	
+	"""
+	all = []
+	conn = get_conn()
+	for q in conn.get_all_queues():
+		all.append(q.name)
+	resp = json.dumps(all)
+	return Response(response=resp, mimetype="application/json") 
+
+@app.route("/queues", methods=['POST'])
+def queues_create():
+	"""
+	Crete queue
+	
+	"""
+
+	conn = get_conn()
+	body = request.get_json(force=True)
+	name = body['name']
+	queue = conn.create_queue(name, 120)
+	resp = "Queue "+name+" has been created\n"
+	return Response(response=resp, mimetype="application/json")
+
+@app.route("/queues/<name>", methods=['DELETE'])
+def queues_remove(name):
+	"""
+	Delete queue
+	
+	"""
+
+	conn = get_conn()
+	queue = conn.get_queue(name)
+	conn.delete_queue(queue)
+	
+	resp = "Queue "+name+" has been removed\n"
+	return Response(response=resp, mimetype="application/json")
+
+@app.route("/queues/<name>/msgs/count", methods=['GET'])
+def messages_count(name):
+	"""
+	Get message count for queue
+	"""
+
+	conn = get_conn()
+	queue = conn.get_queue(name)
+	count = queue.count()
+	
+	resp = "Queue "+name+" has "+str(count)+" messages\n"
+	return Response(response=resp, mimetype="application/json")	
+
+@app.route("/queues/<name>/msgs", methods=['POST'])
+def messages_write(name):
+	"""
+	Write message to queue
+	"""
+
+	body = request.get_json(force=True)
+	messageText = body['content']
+	
+	conn = get_conn()
+	queue = conn.get_queue(name)
+	queue.set_message_class(Message)
+	m = Message()
+	m.set_body(messageText)
+	queue.write(m)
+	
+	resp = "Message "+messageText+" has been written to queue "+name+"\n"
+	return Response(response=resp, mimetype="application/json")	
 
 
+@app.route("/queues/<name>/msgs", methods=['GET'])
+def messages_read(name):
+	"""
+	Get message from queue
+	"""
 
-@app.route('/queues/<name>/msgs', methods=['GET'])
-def read_message(name):
+	conn = get_conn()
+	queue = conn.get_queue(name)
+	messages = queue.get_messages()
+	if len(messages) > 0:
+		message = messages[0]
+		resp = "Queue: "+name+". \nMessage: "+ message.get_body()+"\n"
+	else:
+		resp = "No messages for queue "+name+"\n"
+	return Response(response=resp, mimetype="application/json")
 
-    conn = connect()
-    queue = conn.get_queue(name)
-    
-    # Read the first message
-    if queue is not None:
-        message = queue.read()
-        return "Messgae: " + message.get_body()
-    else:
-        return "Queue could not be found"
+@app.route("/queues/<name>/msgs", methods=['DELETE'])
+def messages_consume(name):
+	"""
+	Consume message from queue
+	"""
 
+	conn = get_conn()
+	queue = conn.get_queue(name)
+	messages = queue.get_messages()
+	if len(messages) > 0:
+		message = messages[0]
+		resp = "Queue: "+name+" \nDeleted message: "+ message.get_body()+" \n"
+		queue.delete_message(message)
+	else:
+		resp = "No messages for queue "+name+"\n"
+	return Response(response=resp, mimetype="application/json")
 
-@app.route('/queues/<name>/msgs', methods=['DELETE'])
-def consume_message(name):
-
-    conn = connect()
-    queue = conn.get_queue(name)
-
-    # Consume the message
-    if queue is not None:
-        message = queue.read()
-        return "Message consumed: " + message.get_body()
-    else:
-        return "Queue could not be found"
-
-
+def get_conn():
+	key_id, secret_access_key = urllib2.urlopen("http://ec2-52-30-7-5.eu-west-1.compute.amazonaws.com:81/key").read().split(':')
+	return boto.sqs.connect_to_region("eu-west-1", aws_access_key_id=key_id ,aws_secret_access_key=secret_access_key)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0",port=5000, debug=True)
